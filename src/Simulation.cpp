f0ebbe082325d3855688b1f13c196ba8ee78c660
IdoNaim <idonai@post.bgu.ac.il>
2024-12-03 19:56:12 +0200

    continued implementing

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,19 +1,46 @@
 #include "../include/Simulation.h"
-using std::string;
-#include <string>
-using std::vector;
+#include "../include/Action.h"
+#include <iostream>
+using namespace std;
 
-void start();
 
-void addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy);
+Simulation::Simulation(const string &configFilePath){
+    //TODO: finish method
+}
 
-void addAction(BaseAction *action);
+void Simulation::start(){
+    cout<<"The simulation has started"<<endl;
+    isRunning = true;
+//TODO: finish method
+}
 
-bool addSettlement(Settlement *settlement);
-bool addFacility(FacilityType facility);
-bool isSettlementExists(const string &settlementName);
-Settlement &getSettlement(const string &settlementName);
-Plan &getPlan(const int planID);
-void step();
-void close();
-void open();
+void Simulation::addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy){
+    Plan p(this->planCounter,settlement,selectionPolicy,this->facilitiesOptions);
+    this->plans.push_back(p);
+
+    AddPlan* a = new AddPlan(settlement.getName(), selectionPolicy->toString());
+    addAction(a);
+  //  delete a; 
+  //dont know if needed
+
+}
+
+void Simulation::addAction(BaseAction *action){
+    this->actionsLog.push_back(action);
+}
+
+bool Simulation::addSettlement(Settlement *settlement);
+
+bool Simulation::addFacility(FacilityType facility);
+
+bool Simulation::isSettlementExists(const string &settlementName);
+
+Settlement & Simulation::getSettlement(const string &settlementName);
+
+Plan & Simulation::getPlan(const int planID);
+
+void Simulation::step();
+
+void Simulation::close();
+
+void Simulation::open();
